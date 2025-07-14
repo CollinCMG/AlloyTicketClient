e7cec0a0cb550ed9d2110ebb71f7ff449195a21d
Collin Buus <CBuus@catholicmutual.org>
2025-07-14 14:38:55 -0500

    add ability to hide pages

--- AlloyTicketClient/Models/PageDto.cs
+++ AlloyTicketClient/Models/PageDto.cs
@@ -7,6 +7,9 @@ namespace AlloyTicketClient.Models
         public string PageName { get; set; } = string.Empty;
         public int PageRank { get; set; }
         public List<IPageItem> Items { get; set; } = new();
+
+        // Returns true if all FieldInputDto items are hidden (IsHidden == true), false otherwise
+        public bool IsHidden => Items.OfType<FieldInputDto>().Any() && Items.OfType<FieldInputDto>().All(f => f.IsHidden);
     }
 
     public interface IPageItem
